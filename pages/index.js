2943c2177cdacdb04204ef1069f340d606ea05c8
Gelli Krishna Murthy Kowsik <jaikrishnagelli@gmail.com>
2020-06-21 15:10:04 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,23 @@ import Layout from '../components/Layout';
 import web3 from '../web3.js';
 import Storage from '../ethereum/build/contracts/Storage.json';
 import Error from './_error.js'
+import StorageInstance from '../instance.js';
 class App extends Component{
     static async getInitialProps(){
         let errorOccured = false;
-        let accounts,networkId,StorageDeployedAddress,StorageInstance,name;
+        let accounts,name;
         try{
             accounts = await web3.eth.getAccounts();
-            networkId = await web3.eth.net.getId();
-            StorageDeployedAddress = Storage.networks[networkId].address;
-            StorageInstance = await new web3.eth.Contract(Storage.abi, StorageDeployedAddress);
             name = await StorageInstance.methods.name().call();
         }catch(err){
             
               console.log(error);
               errorOccured=true;
         }
-        return {accounts,StorageInstance,name};
+        return {accounts,name,errorOccured};
     }
     render(){
-        const {accounts,StorageInstance,name,errorOccured} = this.props;
+        const {accounts,name,errorOccured} = this.props;
         if(errorOccured){
             return <Error />
         }
